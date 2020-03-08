cda3cbaffd89601fba66cb370a58520279ee2663
iChue <mrwarin@gmail.com>
2020-03-09 00:08:40 +0700

    add unit tests and README

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 # NodeJS - Town Delivery Service
 
+![demo](./assets/demo.gif)
+
+Star City decide to introduce a new delivery services to the market in order to support growth of
+E-commerce business in Thailand. Due to innovative nature of the company, the ways to use their
+service is very innovative one.
+
+To use Star City Delivery Service, their customers have to define the delivery route by themselves.
+They can construct it by choosing multiple routes between two towns that Star City provided.
+The delivery cost is equal to the summation of these routes that they choosed.
+Each routes in the list is only ‘one-way’, That is, a route from town A to town B does not imply the
+existence of a route from town B to town A. Even if both of these routes do exist, they are distinct and
+are not necessarily have the same cost.
+
+The purpose of this assignments is to help Star City building the system that provide their customers with
+information about delivery route. you will compute the delivery cost of a certain route, the number of
+possible delivery route between two towns, and the cost of cheapest delivery route between two
+towns.
+## Features
+- Calculate the delivery cost of the given delivery route
+
+![Calculate the delivery cost of the given delivery route](./assets/route-finding.png)
+
+- Calculate the number of possible delivery route that can be construct by the given conditions.
+
+![Calculate the number of possible delivery route that can be construct by the given conditions](./assets/possible-routes.png)
+
+- Calculate the cheapest delivery route between two town.
+![Calculate the cheapest delivery route between two town](./assets/cheapest-route.png)
+
 ## Usages
 ```
 node cmd.js
@@ -10,7 +39,7 @@ node cmd.js
 AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1
 ```
 
-### Support (OS Terminals)
+## Support (OS Terminals)
 see more details on [inquirer.js](https://github.com/SBoudrias/Inquirer.js)
 
 #### Mac OS:
@@ -27,9 +56,9 @@ see more details on [inquirer.js](https://github.com/SBoudrias/Inquirer.js)
 - gnome-terminal (Terminal GNOME)
 - konsole
 
---- 
 
 ## Development
+
 ### Unit Tests
 - required `mocha` to be installed with `npm install`
 
--- test/lib/route.test.js
+++ test/lib/route.test.js
@@ -1,38 +1,109 @@
-const { calculateCheapestRoute } = require('../../lib/route');
+const { calculateCheapestRoute, findPossibleRoute, findingRoute } = require('../../lib/route');
 const assert = require('assert');
-describe('lib/route - calculateCheapestRoute', function () {
-    let routesMap = {
-        A: { edges: { B: 5, D: 15 } },
-        B: { edges: { A: 5, C: 5 } },
-        C: { edges: { B: 5, D: 7 } },
-        D: { edges: { A: 15, C: 7 } },
-        // ---------
-        E: { edges: { F: 5 } },
-        F: { edges: { E: 5, G: 5 } },
-        G: { edges: { F: 5, H: 10, J: 20 } },
-        H: { edges: { G: 10, I: 10 } },
-        I: { edges: { H: 10, J: 5 } },
-        J: { edges: { G: 20 } },
+
+
+describe('lib/route - findingRoute', function () {
+    const routesMap = {
+        A: { edges: { B: 1, C: 4, D: 10 } },
+        B: { edges: { E: 3 } },
+        C: { edges: { D: 4, F: 2 } },
+        D: { edges: { E: 1 } },
+        E: { edges: { A: 2, B: 3 } },
+        F: { edges: { D: 1 } },
     };
     it('Should get results for valid routes', function () {
-        // A -> B
-        assert.deepEqual(calculateCheapestRoute(routesMap, 'A', 'B'), { cost: 5, from: 'A', route: 'A->B', stops: 0});
-        // B -> A
-        assert.deepEqual(calculateCheapestRoute(routesMap, 'B', 'A'), { cost: 5, from: 'B', route: 'B->A', stops: 0});
-        // A -> B -> C
-        assert.deepEqual(calculateCheapestRoute(routesMap, 'A', 'C'), { cost: 10, stops: 1, from: 'A', route:'A->B->C'});
-        // E -> F -> G -> J = 30 < E -> F -> H -> I -> J = 35
-        assert.deepEqual(calculateCheapestRoute(routesMap, 'E', 'J'), { cost: 30, stops: 2, from: 'E', route: 'E->F->G->J'});
-        // E -> F -> H -> I = 30
-        assert.deepEqual(calculateCheapestRoute(routesMap, 'E', 'I'), { cost: 30, stops: 3, from: 'E', route: 'E->F->G->H->I'});
-        // I -> J = 5
-        assert.deepEqual(calculateCheapestRoute(routesMap, 'I', 'J'), { cost: 5, stops: 0, from: 'I', route: 'I->J'});
+        [
+            { input: [routesMap, 'A-B-E'], expected: 4 },
+            { input: [routesMap, 'A-D'], expected: 10 },
+            { input: [routesMap, 'E-A-C-F'], expected: 8 },
+            { input: [routesMap, 'C-D-E-B'], expected: 8 },
+            { input: [routesMap, 'C-D-E-A'], expected: 7 },
+        ].forEach(it => {
+            const { input: [routes, path], expected } = it;
+            assert.equal(findingRoute(routes, path), expected);
+        });
+    });
+
+    it('Should get cost = `0` for invalid conditions or loop itself', function () {
+        [
+            { input: [routesMap, 'A-D-F'] },
+            { input: [routesMap, 'A-B-C'] },
+            { input: [routesMap, 'F-F'] },
+        ].forEach(it => {
+            const { input: [routes, path] } = it;
+            assert(
+                findingRoute(routes, path) === 0,
+                'invalid condition should get `0` possible routes'
+            );
+        });
+    });
+});
+
+describe('lib/route - findPossibleRoute', function () {
+    const routesMap = {
+        A: { edges: { B: 1, C: 4, D: 10 } },
+        B: { edges: { E: 3 } },
+        C: { edges: { D: 4, F: 2 } },
+        D: { edges: { E: 1 } },
+        E: { edges: { A: 2, B: 3 } },
+        F: { edges: { D: 1 } },
+    };
+    it('Should get results for valid routes', function () {
+        [
+            { input: [routesMap, 'A', 'F', 10, 0, 1], expected: 1 },
+            { input: [routesMap, 'A', 'F', 0, 5, 1], expected: 3 },
+            { input: [routesMap, 'E', 'D', 0, 4, 1], expected: 4 },
+            { input: [routesMap, 'E', 'E', 0, 0, 1], expected: 5 },
+            { input: [routesMap, 'E', 'E', 20, 0, 2], expected: 29 },
+        ].forEach(it => {
+            const { input: [routes, start, stop, lessThanCost, maxStops, visitRoute], expected } = it;
+            assert.equal(findPossibleRoute(routes, start, stop, lessThanCost, maxStops, visitRoute).length, expected);
+        });
+    });
+
+    it('Should get `0` possible routes for invalid conditions', function () {
+        [
+            { input: [routesMap, 'B', 'E', 1, 1, 1] },
+            { input: [routesMap, 'B', 'E', 3, 0, 1] },
+            { input: [routesMap, 'E', 'E', 3, 1, 1] },
+        ].forEach(it => {
+            const { input: [routes, start, stop, lessThanCost, maxStops, visitRoute] } = it;
+            assert(
+                findPossibleRoute(routes, start, stop, lessThanCost, maxStops, visitRoute).length === 0,
+                'invalid condition should get `0` possible routes'
+            );
+        });
+    });
+});
+
+describe('lib/route - calculateCheapestRoute', function () {
+    const routesMap = {
+        A: { edges: { B: 1, C: 4, D: 10 } },
+        B: { edges: { E: 3 } },
+        C: { edges: { D: 4, F: 2 } },
+        D: { edges: { E: 1 } },
+        E: { edges: { A: 2, B: 3 } },
+        F: { edges: { D: 1 } },
+    };
+    it('Should get results for valid routes', function () {
+        [
+            { input: [routesMap, 'E', 'D'], expected: { cost: 9, stops: 3, from: 'E', route: 'E->A->C->F->D' } },
+            { input: [routesMap, 'E', 'E'], expected: { cost: 6, stops: 1, from: 'E', route: 'E->B->E', } },
+            { input: [routesMap, 'A', 'F'], expected: { cost: 6, stops: 1, from: 'A', route: 'A->C->F', } },
+            { input: [routesMap, 'C', 'C'], expected: { cost: 11, stops: 3, from: 'A', route: 'C->D->E->A->C', } },
+        ].forEach(it => {
+            const { input: [routes, start, stop], expected } = it;
+            assert.deepEqual(calculateCheapestRoute(routes, start, stop), expected);
+        });
     })
 
-    it('Should get error for invalid routes', function () {
-        assert.equal(calculateCheapestRoute(routesMap, 'A', 'E'), null);
-        assert.equal(calculateCheapestRoute(routesMap, 'E', 'C'), null);
-        assert.equal(calculateCheapestRoute(routesMap, 'E', 'D'), null);
-        assert.equal(calculateCheapestRoute(routesMap, 'J', 'A'), null);
+    it('Should get `null` for invalid routes', function () {
+        [
+            { input: [routesMap, 'I', 'I'] },
+            { input: [routesMap, 'C', 'Z'] },
+        ].forEach(it => {
+            const { input: [routes, start, stop] } = it;
+            assert(calculateCheapestRoute(routes, start, stop) === null, 'invalid route should be null');
+        });
     })
 });
